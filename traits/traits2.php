91c2c58b5eb03560b93303e5be33bae6a692bbdb
buan <sbxcuseme000@gmail.com>
2018-03-25 00:45:40 +0530

    test commit

---
+++ traits/traits2.php
@@ -0,0 +1,37 @@
+<?php
+/* method overriding in traits*/
+/* class Base{
+    public function abc(){
+        echo "In abc method of class Base";
+    }
+}
+
+class child extends Base{
+
+}
+
+$b=new Child();
+$b->abc(); */
+
+/* Here in the above example,so far the abc method of Base class is inherited insdie Child Class
+Now we wil use trait having same function
+*/
+class Base{
+    public function abc(){
+        echo "In abc method of class Base";
+    }
+}
+trait test{
+    public function abc(){
+        echo "In abc method of trait test";
+    } 
+}
+class child extends Base{
+use test;
+}
+
+$b=new Child();
+$b->abc(); 
+
+/*
+When we have same methods in Base class and trait, the trait methods always gets precedence. If the method is present in the child class,then obviously child class gets precedence
