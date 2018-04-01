7dad2313f1b6822aaadaddf1ab64d0ecdd19a584
buan <sbxcuseme000@gmail.com>
2018-04-01 13:43:22 +0530

    test commit

---
+++ latestatickeyword.php
@@ -0,0 +1,5 @@
+<?php
+/**
+ * In normal circumstances when a program runs, we need to compile the program
+ * 
+ */
--- statickeyword.php
+++ statickeyword.php
@@ -1,19 +1,24 @@
 <?php
-class xyz{
-    //create a static variable
-    public static $data="testdata";
 
-    //Create a static method
-    public static function test(){
-        return self::$data;
+class xyz{
+    public static $name;
+    public static function myfunc($name){
+        self::$name=$name;
+        echo self::$name;
     }
 }
 
-//call a static object
-echo xyz::$data."<br>";
+class abc extends xyz{
+public static function myfunc($name){
+    parent::myfunc("Soumya");
+    self::$name=$name;
+    return self::$name;
+    }
+}
+echo abc::myfunc("Banerjee");
 
-//call static method
-echo xyz::test();
+
+//SoumyaBanerjee
 
 /**
  * In case of non static method we sue this function
@@ -30,5 +35,41 @@ echo xyz::test();
  * return self::$data
  * 
  * }
+ * eg.
  * 
+ * class xyz{
+    //create a static variable
+    public static $data="testdata";
+
+    //Create a static method
+    public static function test(){
+        return self::$data;
+    }
+}
+
+//call a static object
+echo xyz::$data."<br>";
+
+//call static method
+echo xyz::test();
+
+ * 
+ * use of static keyword
+ * 1)to count the number of objects
+ * 
+ * 
+ * class xyz{
+    public static $count=0;
+    public static function showcount(){
+        return self::$count;
+    }
+    public function __construct(){
+        self::$count++;
+    }
+  
+}
+$a=new xyz();
+$b=new xyz();
+$c=new xyz();
+echo xyz::showcount();
  */
